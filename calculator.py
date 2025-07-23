6f1153b978fd20bb8ffec674165889a2cb40016a
ShreyaSingh-code <shreya.ss.singh67@gmail.com>
2025-07-23 12:07:42 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,34 @@
+def add(n1,n2):
+    return n1+n2
+def subtract(n1,n2):
+    return n1-n2
+def multiply(n1,n2):
+    return n1*n2
+def divide(n1,n2):
+    return n1/n2 
+
+     
+
+n1 = int(input("Enter first number: "))
+is_continue = True
+while is_continue:
+    operation = input("enter you operation  (+ , _ , * , /) : ")
+    n2 = int(input("Enter your second number : "))
+    if operation == "+":
+        result = add(n1,n2)
+    if operation == "-":
+        result = subtract(n1,n2)    
+    if operation == "*":
+        result = multiply(n1,n2)
+    if operation == "/":
+        result = divide(n1,n2) 
+    else:
+        print("invalid operation")
+    print(f" result: {result}") 
+    more = input("do you want to continue?: ").lower()
+    if more == "yes":
+        n1 = result
+    else:
+        is_continue = False
+
+    
